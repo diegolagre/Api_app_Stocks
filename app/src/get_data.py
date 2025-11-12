34fbd52ef25fdc87fd8fd917b36f5586cc19514e
Diego La Greca <dieguito@Diegos-MacBook-Air.local>
2025-11-12 16:03:02 -0300

    mejorando

--- app/src/get_data.py
+++ app/src/get_data.py
@@ -1,8 +1,8 @@
 import yfinance as yf
 import pandas as pd
 from datetime import datetime
-# from constants.constants import stocks_list
-import aap_stocks.src.stocks_list as stocks_list
+from app.constants import stocks_list
+# import aap_stocks.src.stocks_list as stocks_list
 import os
 
 def get_stock_data(tickers):
@@ -26,7 +26,7 @@ def get_stock_data(tickers):
     return pd.DataFrame(results)
 
 # Lista de tickers
-tickers_list = ("AAPL", "GOOGL", "NVDA", "SAN" ) # stocks_list.stocks_list
+tickers_list = ("AAPL", "GOOGL", "NVDA", "SAN" ) # stocks_list
 
 print(tickers_list)
 
